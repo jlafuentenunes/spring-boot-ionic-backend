25919909ed36ebfdf47bd8487cceb443c35a905f
jlafuentenunes <jlafuentenunes@gmail.com>
2018-08-10 12:20:08 +0100

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/cursomc/services/ClienteService.java
+++ src/main/java/com/cursomc/services/ClienteService.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.cursomc.domain.Cliente;
-import com.cursomc.domain.Cliente;
 import com.cursomc.dto.ClienteDTO;
 import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.services.exceptions.DataIntegrityException;
